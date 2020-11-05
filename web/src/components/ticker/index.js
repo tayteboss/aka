feeb8a2ebd25e1266f50b0422dc647fb21d730f7
Tayte Boss <boss.tayte@gmail.com>
2020-11-05 21:37:26 +1100

    ticker hover fix

--- web/src/components/ticker/index.js
+++ web/src/components/ticker/index.js
@@ -67,7 +67,7 @@ const StripWrapper = styled.section`
         width: auto;
     }
 
-    .styles_ticker__8iZE9:hover {
+    .styles_ticker__8iZE9:hover .styles_ticker-list__1YeNe {
         animation-play-state: running;
     }
 `;
